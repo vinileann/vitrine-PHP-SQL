c3481b797ba2658c59101bbce150075e6d598d9e
Vinicius Leandro <vinileann@gmail.com>
2025-01-13 13:46:17 -0300

    Update logout.php

--- public/src/logout.php
+++ public/src/logout.php
@@ -2,31 +2,6 @@
 
 <?php
     session_start();
-    unset($_SESSION["usuario"]);
-?>
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Vitrine by vinilean</title>
-        
-        <!-- estilo -->
-        <link href="_css/estilo.css" rel="stylesheet">
-    </head>
-
-    <body>
-        <?php include_once("../_incluir/topo.php"); ?>
-        <?php include_once("../_incluir/funcoes.php"); ?>
-        
-        <main>
-
-        </main>
-
-        <?php include_once("../_incluir/rodape.php"); ?>
-    </body>
-</html>
-
-<?php
-    // Fechar conexao
-    mysqli_close($conecta);
+    unset($_SESSION["user_portal"]);
+    header("location:login.php");
 ?>
