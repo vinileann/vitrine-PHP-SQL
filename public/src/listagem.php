c56744c986786b9c8fb783cb87dbf63a8ebe4c9e
Vinicius Leandro <vinileann@gmail.com>
2025-01-13 12:03:14 -0300

    Update listagem.php

--- public/src/listagem.php
+++ public/src/listagem.php
@@ -1,6 +1,11 @@
 <?php require_once("../../conexao/conexao.php"); ?>
 
 <?php
+    session_start();
+    if ( !isset($_SESSION["user_portal"]) ) {
+        header("location:login.php");
+    }
+
     // Determinar localidade BR
     setlocale(LC_ALL, 'pt_BR');
 
