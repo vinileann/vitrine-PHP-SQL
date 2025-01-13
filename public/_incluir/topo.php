51ae692c93646a9c1497282e252032f2e01c9d53
Vinicius Leandro <vinileann@gmail.com>
2025-01-13 13:46:40 -0300

    Update topo.php

--- public/_incluir/topo.php
+++ public/_incluir/topo.php
@@ -1,5 +1,25 @@
 <header>
     <div id="header_central">
+        <?php
+            if ( isset($_SESSION["user_portal"]) ) {
+                $user = $_SESSION["user_portal"];
+                $saudacao = "SELECT nomecompleto ";
+                $saudacao .= " FROM clientes ";
+                $saudacao .= " WHERE clienteID = {$user} ";
+                $saudacao_login = mysqli_query($conecta, $saudacao);
+                if ( !$saudacao_login ) {
+                    die("Falha no banco");
+                }
+                $saudacao_login = mysqli_fetch_assoc($saudacao_login);
+                $nome = $saudacao_login["nomecompleto"];
+        ?>
+        <div id="header_saudacao">
+            <h5>Bem vindo, <?php echo $nome ?> | <a href="logout.php"> SAIR </a> </h5>
+        </div>
+        <?php
+            }
+        ?>
+
         <img src="../_assets/logo_andes.gif">
         <img src="../_assets/text_bnwcoffee.gif">
     </div>
